12b65c88fd2778254f09b73b8220c40f32c5b882
nepeat <nepeat@gmail.com>
2025-05-10 20:32:43 -0700

    add version + urls

--- build/market-helpers.js
+++ build/market-helpers.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         sillymarket helpers!
 // @namespace    http://tampermonkey.net/
-// @version      2025-05-11
+// @version      0.0.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://sillypost.net/games/sillyexchange
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=sillypost.net
 // @grant        none
+// @updateURL    https://raw.githubusercontent.com/nepeat/sillypost-userscripts/refs/heads/main/build/market-helpers.js
+// @downloadURL  https://raw.githubusercontent.com/nepeat/sillypost-userscripts/refs/heads/main/build/market-helpers.js
 // ==/UserScript==
 
 const getBalance = async () => {
